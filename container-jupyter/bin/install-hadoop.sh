53821152362444f725b169745106b499874e2ba1
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-11-29 11:02:10 +0100

    custom jupyter notebook

---
+++ container-jupyter/bin/install-hadoop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Exit immediately if a command exits with a non-zero status
+set -e
+
+echo "Creating Hadoop users..."
+addgroup hadoop
+adduser --disabled-password --gecos "" --ingroup hadoop hduser
+
+echo "Disabling IPv6 (not supported by Hadoop)..."
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+
+echo "Fetching Hadoop..."
+HADOOP_DOWNLOAD_URL=http://apache.mirrors.spacedump.net/hadoop/common/hadoop-2.6.0/hadoop-2.6.0.tar.gz
+HADOOP_TGZ=${HADOOP_DOWNLOAD_URL##*/}
+HADOOP_PACKAGE_NAME=${HADOOP_TGZ%.tar.gz}
+wget -q $HADOOP_DOWNLOAD_URL -O /tmp/$HADOOP_TGZ
+
+echo "Installing $HADOOP_PACKAGE_NAME..."
+mkdir /opt/hadoop/
+tar xzf /tmp/$HADOOP_TGZ -C /opt/hadoop/
+ln -s /opt/hadoop/$HADOOP_PACKAGE_NAME /opt/hadoop/default
+chown hduser:hadoop -R /opt/hadoop/
+
+echo "$HADOOP_PACKAGE_NAME installation complete."
